823b019be1d9905a53dbfe7480ea25137b155275
Yahya <92686055+ya7yaq@users.noreply.github.com>
2022-01-11 22:08:00 +0300

    organize the code and update some parts

--- src/App.js
+++ src/App.js
@@ -4,55 +4,13 @@ import './App.css';
 import LogReg from './components/LogReg';
 
 function App() {
-    const [show, setshow] = useState(false);
-
-    const aa = () => {
-        setshow(true);
-    };
     return (
         <div className="App">
             {/* Start Div App */}
 
-            <div>
-                <div className="has-text-centered logRedbtns">
-                    <span>
-                        <button
-                            onClick={() => {
-                                {
-                                    aa();
-                                }
-                            }}>
-                            Login
-                        </button>
-                    </span>
-                    <div>
-                        <span>
-                            <input
-                                className="inputRadio"
-                                name="aa"
-                                type="radio"
-                                id="log"
-                                onClick={() => {
-                                    {
-                                        aa();
-                                    }
-                                }}
-                            />
-                            <label className="RadioLabel" htmlFor="log">
-                                loooog
-                            </label>
-                        </span>
-                        <span>
-                            <input className="inputRadio" name="aa" type="radio" id="reg" defaultChecked />
-                            <label className="RadioLabel" htmlFor="reg">
-                                loooog
-                            </label>
-                        </span>
-                    </div>
-                    {show ? <LogReg /> : null}
-                </div>
+            <div className="card">
+                <LogReg />
             </div>
-
             {/* End Div App */}
         </div>
     );
--- src/components/LogReg.js
+++ src/components/LogReg.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.min.css';
 import '../App.css';
 import defaultImg from '../assets/images/default img.png';
@@ -7,33 +7,52 @@ import ss from '../assets/images/anime-girl.gif';
 function LogReg() {
     return (
         <div>
-            <div className="card">
-                <form className="has-text-centered card-content">
-                    <div className="profileimg">
-                        <img className="is-rounded" src={ss} alt="profile img" />
+            {/* start form here */}
+            <form className="has-text-centered card-content p-4">
+                <div className="profileimg">
+                    <img className="is-rounded" src={ss} />
+                </div>
+                {/* log and register button Up  */}
+                <div className="has-text-centered logRedbtns  my-5">
+                    <div>
+                        {/* log button  */}
+                        <span>
+                            <input className="inputRadio" name="aa" type="radio" id="log" onClick={() => {}} />
+                            <label className="RadioLabel" htmlFor="log">
+                                loooog
+                            </label>
+                        </span>
+                        {/* register button  */}
+                        <span>
+                            <input className="inputRadio" name="aa" type="radio" id="reg" defaultChecked />
+                            <label className="RadioLabel" htmlFor="reg">
+                                loooog
+                            </label>
+                        </span>
                     </div>
-                    {/*  */}
-                    {/*  */}
-                    <div className="has-text-left mt-6">
-                        <label htmlFor="Fname">Frist Name</label>
-                        <input className="input is-rounded noBorder" autoComplete="nope" type="text" id="Fname" />
-                        <br />
-                    </div>
-                    <div className="has-text-left">
-                        <label htmlFor="Lname">Last Name</label>
-                        <input className="input is-rounded noBorder" autoComplete="nope" type="text" id="Lname" /> <br />
-                    </div>
-                    <div className="has-text-left">
-                        <label htmlFor="Email">Email</label>
-                        <input className="input is-rounded noBorder" type="email" id="Email" /> <br />
-                    </div>
-                    <div className="has-text-left">
-                        <label htmlFor="password">Password</label>
-                    </div>
-                    <input id="password" className="input is-rounded noBorder" autoComplete="nope" type="password" /> <br />
-                    <button className="button is-info is-rounded my-2">login</button>
-                </form>
-            </div>
+                </div>
+                {/* All inputs here  */}
+                <div className="has-text-left mt-3">
+                    <label htmlFor="Fname">First Name</label>
+                    <input className="input is-rounded noBorder" autoComplete="nope" type="text" id="Fname" />
+                    <br />
+                </div>
+                <div className="has-text-left">
+                    <label htmlFor="Lname">Last Name</label>
+                    <input className="input is-rounded noBorder" autoComplete="nope" type="text" id="Lname" /> <br />
+                </div>
+                <div className="has-text-left">
+                    <label htmlFor="Email">Email</label>
+                    <input className="input is-rounded noBorder" type="email" id="Email" /> <br />
+                </div>
+                <div className="has-text-left">
+                    <label htmlFor="password">Password</label>
+                </div>
+                <input id="password" className="input is-rounded noBorder" autoComplete="nope" type="password" /> <br />
+                {/* buttons to send data  */}
+                <button className="button is-info is-rounded mt-4">login</button>
+                <button className="button is-info is-rounded mt-4">register</button>
+            </form>
         </div>
     );
 }
