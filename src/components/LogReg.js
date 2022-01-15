2aa11f196d7cd0ceffb8e6d710602e5a58a13254
Yahya <92686055+ya7yaq@users.noreply.github.com>
2022-01-15 16:53:42 +0300

    add onChange to save inputs  user in state

--- src/components/LogReg.js
+++ src/components/LogReg.js
@@ -5,18 +5,29 @@ import defaultImg from '../assets/images/default img.png';
 import ss from '../assets/images/anime-girl.gif';
 
 function LogReg() {
-    const [ConvertBTlogReg, setConvertBTlogReg] = useState('true');
+    const [ConvertBTlogReg, setConvertBTlogReg] = useState('true'); // change form login to register
+    // const [logcheck, setlogcheck] = useState(); // check on login data
 
+    const dataObj = { imgUser: '', FirstName: '', LastName: '', Email: '', Password: '' }; //Data for test
+    const [SaveFormData, setSaveFormData] = useState(dataObj); // save data user in this state
+
+    // function to change ConvertBTlogReg state
     const toRegis = () => {
         setConvertBTlogReg(!ConvertBTlogReg);
     };
+    // function to handle input data
+    const handelInputData = e => {
+        const { name, value } = e.target;
+        setSaveFormData({ ...SaveFormData, [name]: value });
 
+        console.log(SaveFormData);
+    };
     return (
         <div>
             {/* start form here */}
             <form className={` has-text-centered card-content p-4 `}>
                 <div className="profileimg">
-                    <img className="is-rounded" src="" />
+                    <img className="is-rounded" src={SaveFormData.imgUser} />
                 </div>
                 {/* log and register button Up  */}
                 <div className={`has-text-centered logRedbtns  my-5 `}>
@@ -55,28 +66,68 @@ function LogReg() {
                         </span>
                     </div>
                 </div>
-                {/* All inputs here  */}
+                {/*
+                //! Input Start Here 
+                */}
                 <div className={`has-text-left mt-3 ${ConvertBTlogReg ? '' : 'hideElement'}`}>
                     <label htmlFor="Fname">First Name</label>
-                    <input className="input is-rounded noBorder" autoComplete="nope" type="text" id="Fname" />
+                    <input
+                        className="input is-rounded noBorder"
+                        autoComplete="nope"
+                        type="text"
+                        id="Fname"
+                        name="FirstName"
+                        value={SaveFormData.FirstName}
+                        onChange={handelInputData}
+                    />
                     <br />
                 </div>
                 <div className={`has-text-left ${ConvertBTlogReg ? '' : 'hideElement'}`}>
                     <label htmlFor="Lname">Last Name</label>
-                    <input className="input is-rounded noBorder" autoComplete="nope" type="text" id="Lname" /> <br />
+                    <input
+                        className="input is-rounded noBorder"
+                        autoComplete="nope"
+                        type="text"
+                        id="Lname"
+                        name="LastName"
+                        value={SaveFormData.LastName}
+                        onChange={handelInputData}
+                    />
+                    <br />
                 </div>
                 <div className="has-text-left">
                     <label htmlFor="Email">Email</label>
-                    <input className="input is-rounded noBorder" type="email" id="Email" autoComplete="nope" /> <br />
+                    <input
+                        className="input is-rounded noBorder"
+                        type="email"
+                        id="Email"
+                        autoComplete="nope"
+                        name="Email"
+                        value={SaveFormData.Email}
+                        onChange={handelInputData}
+                    />
+
+                    <br />
                 </div>
                 <div className="has-text-left">
                     <label htmlFor="password">Password</label>
                 </div>
-                <input id="password" className="input is-rounded noBorder" autoComplete="nope" type="password" /> <br />
+                <input
+                    id="password"
+                    className="input is-rounded noBorder"
+                    autoComplete="nope"
+                    type="password"
+                    name="Password"
+                    value={SaveFormData.Password}
+                    onChange={handelInputData}
+                />
+                <br />
                 {/* buttons to send data  */}
                 <button
                     className={`button is-info is-rounded mt-4 ${ConvertBTlogReg ? 'hideElement' : ''}`}
-                    onClick={() => {
+                    onClick={e => {
+                        e.preventDefault();
+
                         alert('login done');
                     }}>
                     login
@@ -84,7 +135,8 @@ function LogReg() {
                 <button
                     className={`button is-info is-rounded mt-4 ${ConvertBTlogReg ? '' : 'hideElement'}`}
                     type="submit"
-                    onClick={() => {
+                    onClick={e => {
+                        e.preventDefault();
                         alert('register done');
                     }}>
                     register
